1ed285a1ea148b73603d51d9d528a2b26e1c674f
Edxael <zerphyr.r@gmail.com>
2017-11-09 13:17:23 -0700

    update db.json

--- schemas/mySchema.js
+++ schemas/mySchema.js
@@ -4,25 +4,29 @@ const axios = require('axios')
 
 
 
-const UserType = new GraphQLObjectType({
-  name: 'User',
+const CharacterType = new GraphQLObjectType({
+  name: 'Character',
   fields:{
     id: { type: GraphQLString },
     name: { type: GraphQLString } ,
-    from: { type: GraphQLString },
-    year: { type: GraphQLInt },
-    pic: { type: GraphQLString }
+    race: { type: GraphQLString },
+    employer: { type: GraphQLString },
+    abilities: { type: GraphQLString },
+    gender: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    pic: { type: GraphQLString },
+    gameId: { type: GraphQLString }
   }
 })
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    user: {
-      type: UserType,
+    character: {
+      type: CharacterType,
       args: { id: { type: GraphQLString } },
       resolve( parentValue, args ) {
-        return axios.get(`http://localhost:3000/users/${args.id}`)
+        return axios.get(`http://localhost:3000/characters/${args.id}`)
           .then( (resp)=>{ return resp.data } )    // { data: { id: x, ....... } }
       }
     }
@@ -30,3 +34,5 @@ const RootQuery = new GraphQLObjectType({
 })
 
 module.exports = new GraphQLSchema({ query: RootQuery })
+
+// http://localhost:3000/characters
